fb3dc786521a4d6472366fb6e43d6b2d7f6f539b
VAVAQUIN <0077cam@gmail.com>
2025-01-20 21:45:55 -0300

    Add files via upload

--- modal.js
+++ modal.js
@@ -25,3 +25,41 @@ modal.addEventListener("click", (event) => {
         modal.style.display = "none";
     }
 });
+
+
+
+
+
+const images2 = document.querySelectorAll(".gallery3 img");
+const images3 = document.querySelectorAll("img");
+
+images2.forEach((img) => {
+    img.addEventListener("click", () => {
+        modal.style.display = "flex"; 
+        modalImage.src = img.src; 
+        modalImage.alt = img.alt; 
+    });
+});
+
+
+
+// images3.forEach((img) => {
+//     img.addEventListener("click", () => {
+//         modal.style.display = "flex"; 
+//         modalImage.src = img.src; 
+//         modalImage.alt = img.alt; 
+//     });
+// });
+
+
+// closeModal.addEventListener("click", () => {
+//     modal.style.display = "none";
+// });
+
+
+// modal.addEventListener("click", (event) => {
+//     if (event.target === modal) {
+//         modal.style.display = "none";
+//     }
+// });
+
